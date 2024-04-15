e568155caa8e0b1378d0b20beda5578fe418bf0c
nisharb911 <95846670+nisharb911@users.noreply.github.com>
2024-04-15 16:50:42 +0530

    Add files via upload

---
+++ home.php
@@ -0,0 +1,37 @@
+<?php 
+session_start();
+
+if (isset($_SESSION['id']) && isset($_SESSION['fname'])) {
+ ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Home</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
+	<script type="module" src="./index.js"></script>
+</head>
+<body>
+    <div class="d-flex justify-content-center align-items-center vh-100">
+    	
+    	<div class="shadow w-350 p-3 text-center">
+    		<img src="upload/<?=$_SESSION['pp']?>"
+    		     class="img-fluid rounded-circle">
+            <h3 class="display-4 "><?=$_SESSION['fname']?></h3>
+			<a href="map.html" >&#xf3c5;</a>
+    <!--The div element for the map -->
+            <a href="logout.php" class="btn btn-warning">
+            	Logout
+            </a>
+		</div>
+    </div>
+</body>
+</html>
+
+<?php }else {
+	header("Location: login.php");
+	exit;
+} ?>
